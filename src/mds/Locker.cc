1bc1f41bd524653d48e2d6cc09058a0cac5c41f0
Greg Farnum <greg@inktank.com>
2013-01-15 17:27:25 -0800

    mds: move durable Session bits into session_info_t
    This keeps the on-disk structure explicitly separate from the in-memory
    functional stuff.
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -1587,8 +1587,8 @@ Capability* Locker::issue_new_caps(CInode *in,
   }
 
   // my needs
-  assert(session->inst.name.is_client());
-  int my_client = session->inst.name.num();
+  assert(session->info.inst.name.is_client());
+  int my_client = session->info.inst.name.num();
   int my_want = ceph_caps_for_mode(mode);
 
   // register a capability
@@ -1770,7 +1770,7 @@ void Locker::issue_truncate(CInode *in)
 
 void Locker::revoke_stale_caps(Session *session)
 {
-  dout(10) << "revoke_stale_caps for " << session->inst.name << dendl;
+  dout(10) << "revoke_stale_caps for " << session->info.inst.name << dendl;
   client_t client = session->get_client();
 
   for (xlist<Capability*>::iterator p = session->caps.begin(); !p.end(); ++p) {
@@ -1804,7 +1804,7 @@ void Locker::revoke_stale_caps(Session *session)
 
 void Locker::resume_stale_caps(Session *session)
 {
-  dout(10) << "resume_stale_caps for " << session->inst.name << dendl;
+  dout(10) << "resume_stale_caps for " << session->info.inst.name << dendl;
 
   for (xlist<Capability*>::iterator p = session->caps.begin(); !p.end(); ++p) {
     Capability *cap = *p;
@@ -1821,7 +1821,7 @@ void Locker::resume_stale_caps(Session *session)
 
 void Locker::remove_stale_leases(Session *session)
 {
-  dout(10) << "remove_stale_leases for " << session->inst.name << dendl;
+  dout(10) << "remove_stale_leases for " << session->info.inst.name << dendl;
   xlist<ClientLease*>::iterator p = session->leases.begin();
   while (!p.end()) {
     ClientLease *l = *p;
