31592c4696a7cdf890eeafcb24727eb1d6df8746
Sage Weil <sage@inktank.com>
2013-01-25 15:06:42 -0800

    common/RateMeasure: class for measuring a rate over time
    Keep a short history.  Quantize samples.  Extrapolate when there is limited
    historical data.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

---
+++ src/common/RateMeasure.h
@@ -0,0 +1,105 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_COMMON_RATEMEASURE_H
+#define CEPH_COMMON_RATEMEASURE_H
+
+#include <deque>
+#include "include/utime.h"
+#include "common/Clock.h"
+#include "common/Formatter.h"
+
+namespace ceph {
+
+class RateMeasure {
+  struct TimeSample {
+    time_t stamp;
+    int64_t value;
+    TimeSample(time_t s=0, int64_t v=0) : stamp(s), value(v) {}
+  };
+  std::deque<TimeSample> m_samples;
+  unsigned m_max_samples, m_precision;
+  time_t m_last_get_qt;
+  int64_t m_last_get_value;
+
+public:
+  RateMeasure(unsigned m = 30, unsigned p = 5)
+    : m_max_samples(m),
+      m_precision(p),
+      m_last_get_qt(0),
+      m_last_get_value(0)
+  { }
+
+  void trim(utime_t stamp) {
+    time_t max = stamp.sec() - m_precision * (m_max_samples + 1);
+    while (!m_samples.empty() &&
+	   m_samples.front().stamp < max) {
+      m_samples.pop_front();
+    }
+  }
+
+  void add(utime_t stamp, int64_t value) {
+    time_t offset = stamp.sec() % m_precision;
+    time_t qt = stamp.sec() - offset;
+    if (!m_samples.empty() && m_samples.back().stamp == qt)
+      m_samples.back().value += value;
+    else
+      m_samples.push_back(TimeSample(qt, value));
+    trim(stamp);
+  }
+
+  int64_t get_rate(utime_t now, unsigned duration) {
+    time_t offset = now.sec() % m_precision;
+    time_t qt = now.sec() - offset;
+
+    if (m_samples.size() < 2)
+      return 0;
+
+    if (qt == m_last_get_qt)
+      return m_last_get_value;
+
+    //std::cout << "qt " << qt << " offset " << offset << std::endl;
+    trim(now);
+    int64_t r = 0;
+    int left = duration;
+    time_t min_stamp = 0;
+    std::deque<TimeSample>::const_reverse_iterator p = m_samples.rbegin();
+    for (++p;
+	 left > 0 && p != m_samples.rend();
+	 left -= m_precision, ++p) {
+      //std::cout << " t " << p->stamp << " v " << p->value << std::endl;
+      r += p->value;
+      min_stamp = p->stamp;
+    }
+
+    // scale up overall value if we have limited samples.
+    if (qt - min_stamp < duration) {
+      //std::cout << " scale total " << r << " by " << duration << " / " << (qt - min_stamp) << std::endl;
+      r = r * duration / (qt - min_stamp);
+    }
+
+    // cache result for this time interval
+    m_last_get_qt = qt;
+    m_last_get_value = r;
+    return r;
+  }
+
+  void dump(Formatter *f) const {
+    f->dump_unsigned("max_samples", m_max_samples);
+    f->dump_unsigned("precision", m_precision);
+    f->open_object_section("samples");
+    for (std::deque<TimeSample>::const_iterator p = m_samples.begin();
+	 p != m_samples.end();
+	 ++p) {
+      f->open_object_section("sample");
+      f->dump_unsigned("stamp", p->stamp);
+      f->dump_int("value", p->value);
+      f->close_section();
+    }
+    f->close_section();
+  }
+};
+
+}
+
+#endif
---
+++ src/test/common/RateMeasure.cc
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+
+#include "common/RateMeasure.h"
+
+TEST(RateMeasure, empty) {
+  utime_t now = ceph_clock_now(NULL);
+  RateMeasure rm;
+  ASSERT_EQ(0, rm.get_rate(now, 60));
+}
+
+TEST(RateMeasure, Constant) {
+  RateMeasure rm(60, 5);
+  for (int x = 1; x < 1000; x++)
+    rm.add(utime_t(x,0), 10);
+  ASSERT_EQ(600, rm.get_rate(utime_t(999, 0), 60));
+}
+
+TEST(RateMeasure, Incomplete) {
+  for (int dur = 5; dur < 100; dur++) {
+    RateMeasure rm(60, 5);
+    for (int t = 0; t <= dur; t++)
+      rm.add(utime_t(t, 0), 10);
+    //std::cout << " duration " << dur << std::endl;
+    ASSERT_EQ(600, rm.get_rate(utime_t(dur, 0), 60));
+  }
+}
