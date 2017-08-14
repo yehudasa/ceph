zenglg/ceph-ci

rgw: add rados hints when writing objects
Add it to both system and data objects. When writing data object
we only provide write_size.

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_cache.h
@@ -233,7 +233,7 @@ public:
               bufferlist& data,
               RGWObjVersionTracker *objv_tracker,
               real_time set_mtime);
-  int put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl, off_t ofs, bool exclusive);
+  int put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl, bool exclusive);
 
   int get_system_obj(RGWObjectCtx& obj_ctx, RGWRados::SystemObject::Read::GetObjState& read_state,
                      RGWObjVersionTracker *objv_tracker, rgw_obj& obj,
@@ -424,31 +424,25 @@ int RGWCache<T>::put_system_obj_impl(rgw_obj& obj, uint64_t size, real_time *mti
 }
 
 template <class T>
-int RGWCache<T>::put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& data, off_t ofs, bool exclusive)
+int RGWCache<T>::put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& data, bool exclusive)
 {
   rgw_bucket bucket;
   string oid;
   normalize_bucket_and_obj(obj.bucket, obj.get_object(), bucket, oid);
   ObjectCacheInfo info;
-  bool cacheable = false;
-  if ((ofs == 0) || (ofs == -1)) {
-    cacheable = true;
-    info.data = data;
-    info.meta.size = data.length();
-    info.status = 0;
-    info.flags = CACHE_FLAG_DATA;
-  }
-  int ret = T::put_system_obj_data(ctx, obj, data, ofs, exclusive);
-  if (cacheable) {
-    string name = normal_name(bucket, oid);
-    if (ret >= 0) {
-      cache.put(name, info, NULL);
-      int r = distribute_cache(name, obj, info, UPDATE_OBJ);
-      if (r < 0)
-        mydout(0) << "ERROR: failed to distribute cache for " << obj << dendl;
-    } else {
-      cache.remove(name);
-    }
+  info.data = data;
+  info.meta.size = data.length();
+  info.status = 0;
+  info.flags = CACHE_FLAG_DATA;
+  int ret = T::put_system_obj_data(ctx, obj, data, exclusive);
+  string name = normal_name(bucket, oid);
+  if (ret >= 0) {
+    cache.put(name, info, NULL);
+    int r = distribute_cache(name, obj, info, UPDATE_OBJ);
+    if (r < 0)
+      mydout(0) << "ERROR: failed to distribute cache for " << obj << dendl;
+  } else {
+    cache.remove(name);
   }
 
   return ret;

src/rgw/rgw_rados.cc
@@ -2281,7 +2281,7 @@ int RGWPutObjProcessor_Aio::handle_obj_data(rgw_obj& obj, bufferlist& bl, off_t 
 
   // For the first call pass -1 as the offset to
   // do a write_full.
-  return store->aio_put_obj_data(NULL, obj, bl, ((ofs != 0) ? ofs : -1), exclusive, phandle);
+  return store->aio_put_obj_data(obj, bl, ((ofs != 0) ? ofs : -1), exclusive, phandle);
 }
 
 struct put_obj_aio_info RGWPutObjProcessor_Aio::pop_pending()
@@ -5627,7 +5627,7 @@ read_omap:
   if (write_map) {
     bufferlist new_bl;
     ::encode(m, new_bl);
-    ret = put_system_obj_data(NULL, obj, new_bl, -1, false);
+    ret = put_system_obj_data(NULL, obj, new_bl, false);
     if (ret < 0) {
       ldout(cct, 0) << "WARNING: could not save avail pools map info ret=" << ret << dendl;
     }
@@ -5673,7 +5673,7 @@ int RGWRados::update_placement_map()
 
   bufferlist new_bl;
   ::encode(m, new_bl);
-  ret = put_system_obj_data(NULL, obj, new_bl, -1, false);
+  ret = put_system_obj_data(NULL, obj, new_bl, false);
   if (ret < 0) {
     ldout(cct, 0) << "WARNING: could not save avail pools map info ret=" << ret << dendl;
   }
@@ -6383,6 +6383,10 @@ int RGWRados::Object::Write::_do_write_meta(uint64_t size, uint64_t accounted_si
   op.mtime2(&mtime_ts);
 
   if (meta.data) {
+    uint64_t obj_size = meta.data->length();
+    op.set_alloc_hint2(obj_size, obj_size, ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                                           ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                                           ALLOC_HINT_FLAG_IMMUTABLE);
     /* if we want to overwrite the data, we also want to overwrite the
        xattrs, so just remove the object */
     op.write_full(*meta.data);
@@ -6601,6 +6605,7 @@ int RGWRados::put_system_obj_impl(rgw_obj& obj, uint64_t size, real_time *mtime,
 
   ObjectWriteOperation op;
 
+
   if (flags & PUT_OBJ_EXCL) {
     if (!(flags & PUT_OBJ_CREATE))
 	return -EINVAL;
@@ -6611,6 +6616,9 @@ int RGWRados::put_system_obj_impl(rgw_obj& obj, uint64_t size, real_time *mtime,
     op.create(false);
   }
 
+  op.set_alloc_hint2(data.length(), data.length(), ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                                                   ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                                                   ALLOC_HINT_FLAG_IMMUTABLE);
   if (objv_tracker) {
     objv_tracker->prepare_op_for_write(&op);
   }
@@ -6652,7 +6660,7 @@ int RGWRados::put_system_obj_impl(rgw_obj& obj, uint64_t size, real_time *mtime,
 }
 
 int RGWRados::put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl,
-			       off_t ofs, bool exclusive)
+			          bool exclusive)
 {
   rgw_rados_ref ref;
   rgw_bucket bucket;
@@ -6663,14 +6671,14 @@ int RGWRados::put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl,
 
   ObjectWriteOperation op;
 
-  if (exclusive)
+  if (exclusive) {
     op.create(true);
-
-  if (ofs == -1) {
-    op.write_full(bl);
-  } else {
-    op.write(ofs, bl);
   }
+
+  op.set_alloc_hint2(0, 0, ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                           ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                           ALLOC_HINT_FLAG_IMMUTABLE);
+  op.write_full(bl);
   r = ref.ioctx.operate(ref.oid, &op);
   if (r < 0)
     return r;
@@ -6689,19 +6697,7 @@ int RGWRados::put_system_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl,
  * attrs: all the given attrs are written to bucket storage for the given object
  * Returns: 0 on success, -ERR# otherwise.
  */
-int RGWRados::put_obj_data(void *ctx, rgw_obj& obj,
-			   const char *data, off_t ofs, size_t len, bool exclusive)
-{
-  void *handle;
-  bufferlist bl;
-  bl.append(data, len);
-  int r = aio_put_obj_data(ctx, obj, bl, ofs, exclusive, &handle);
-  if (r < 0)
-    return r;
-  return aio_wait(handle);
-}
-
-int RGWRados::aio_put_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl,
+int RGWRados::aio_put_obj_data(rgw_obj& obj, bufferlist& bl,
 			       off_t ofs, bool exclusive,
                                void **handle)
 {
@@ -6721,6 +6717,16 @@ int RGWRados::aio_put_obj_data(void *ctx, rgw_obj& obj, bufferlist& bl,
     op.create(true);
 
   if (ofs == -1) {
+    /*
+     * not setting size for allocation hint
+     * assuming first write buffer length is the common chunk size that will be used.
+     * Not using configured chunk size, because that might be modified when pool
+     * alignment requires.
+     */
+    ldout(cct, 20) << "setting rados allocation hint, write_size=" << bl.length() << dendl;
+    op.set_alloc_hint2(0, bl.length(), ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                       ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                       ALLOC_HINT_FLAG_IMMUTABLE);
     op.write_full(bl);
   } else {
     op.write(ofs, bl);

yehudasa/ceph

src/rgw/rgw_cache.h
@@ -231,7 +231,7 @@ public:
               bufferlist& data,
               RGWObjVersionTracker *objv_tracker,
               real_time set_mtime) override;
-  int put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl, off_t ofs, bool exclusive,
+  int put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl, bool exclusive,
                           RGWObjVersionTracker *objv_tracker = nullptr) override;
 
   int get_system_obj(RGWObjectCtx& obj_ctx, RGWRados::SystemObject::Read::GetObjState& read_state,
@@ -423,36 +423,30 @@ int RGWCache<T>::put_system_obj_impl(rgw_raw_obj& obj, uint64_t size, real_time 
 }
 
 template <class T>
-int RGWCache<T>::put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& data, off_t ofs, bool exclusive,
+int RGWCache<T>::put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& data, bool exclusive,
                                      RGWObjVersionTracker *objv_tracker)
 {
   rgw_pool pool;
   string oid;
   normalize_pool_and_obj(obj.pool, obj.oid, pool, oid);
   ObjectCacheInfo info;
-  bool cacheable = false;
-  if ((ofs == 0) || (ofs == -1)) {
-    cacheable = true;
-    info.data = data;
-    info.meta.size = data.length();
-    info.status = 0;
-    info.flags = CACHE_FLAG_DATA;
-  }
+  info.data = data;
+  info.meta.size = data.length();
+  info.status = 0;
+  info.flags = CACHE_FLAG_DATA;
   if (objv_tracker) {
     info.version = objv_tracker->write_version;
     info.flags |= CACHE_FLAG_OBJV;
   }
-  int ret = T::put_system_obj_data(ctx, obj, data, ofs, exclusive, objv_tracker);
-  if (cacheable) {
-    string name = normal_name(pool, oid);
-    if (ret >= 0) {
-      cache.put(name, info, NULL);
-      int r = distribute_cache(name, obj, info, UPDATE_OBJ);
-      if (r < 0)
-        mydout(0) << "ERROR: failed to distribute cache for " << obj << dendl;
-    } else {
-      cache.remove(name);
-    }
+  int ret = T::put_system_obj_data(ctx, obj, data, exclusive, objv_tracker);
+  string name = normal_name(pool, oid);
+  if (ret >= 0) {
+    cache.put(name, info, NULL);
+    int r = distribute_cache(name, obj, info, UPDATE_OBJ);
+    if (r < 0)
+      mydout(0) << "ERROR: failed to distribute cache for " << obj << dendl;
+  } else {
+    cache.remove(name);
   }
 
   return ret;

src/rgw/rgw_rados.cc
@@ -2479,7 +2479,7 @@ int RGWPutObjProcessor_Aio::handle_obj_data(rgw_raw_obj& obj, bufferlist& bl, of
 
   // For the first call pass -1 as the offset to
   // do a write_full.
-  return store->aio_put_obj_data(NULL, obj, bl, ((ofs != 0) ? ofs : -1), exclusive, phandle);
+  return store->aio_put_obj_data(obj, bl, ((ofs != 0) ? ofs : -1), exclusive, phandle);
 }
 
 struct put_obj_aio_info RGWPutObjProcessor_Aio::pop_pending()
@@ -6025,7 +6025,7 @@ read_omap:
   if (write_map) {
     bufferlist new_bl;
     ::encode(m, new_bl);
-    ret = put_system_obj_data(NULL, obj, new_bl, -1, false);
+    ret = put_system_obj_data(NULL, obj, new_bl, false);
     if (ret < 0) {
       ldout(cct, 0) << "WARNING: could not save avail pools map info ret=" << ret << dendl;
     }
@@ -6081,7 +6081,7 @@ int RGWRados::update_placement_map()
 
   bufferlist new_bl;
   ::encode(m, new_bl);
-  ret = put_system_obj_data(NULL, obj, new_bl, -1, false);
+  ret = put_system_obj_data(NULL, obj, new_bl, false);
   if (ret < 0) {
     ldout(cct, 0) << "WARNING: could not save avail pools map info ret=" << ret << dendl;
   }
@@ -6861,6 +6861,10 @@ int RGWRados::Object::Write::_do_write_meta(uint64_t size, uint64_t accounted_si
   op.mtime2(&mtime_ts);
 
   if (meta.data) {
+    uint64_t obj_size = meta.data->length();
+    op.set_alloc_hint2(obj_size, obj_size, ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                                           ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                                           ALLOC_HINT_FLAG_IMMUTABLE);
     /* if we want to overwrite the data, we also want to overwrite the
        xattrs, so just remove the object */
     op.write_full(*meta.data);
@@ -7086,6 +7090,7 @@ int RGWRados::put_system_obj_impl(rgw_raw_obj& obj, uint64_t size, real_time *mt
 
   ObjectWriteOperation op;
 
+
   if (flags & PUT_OBJ_EXCL) {
     if (!(flags & PUT_OBJ_CREATE))
 	return -EINVAL;
@@ -7096,6 +7101,9 @@ int RGWRados::put_system_obj_impl(rgw_raw_obj& obj, uint64_t size, real_time *mt
     op.create(false);
   }
 
+  op.set_alloc_hint2(data.length(), data.length(), ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                                                   ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                                                   ALLOC_HINT_FLAG_IMMUTABLE);
   if (objv_tracker) {
     objv_tracker->prepare_op_for_write(&op);
   }
@@ -7137,7 +7145,7 @@ int RGWRados::put_system_obj_impl(rgw_raw_obj& obj, uint64_t size, real_time *mt
 }
 
 int RGWRados::put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
-                                  off_t ofs, bool exclusive,
+                                  bool exclusive,
                                   RGWObjVersionTracker *objv_tracker)
 {
   rgw_rados_ref ref;
@@ -7148,17 +7156,18 @@ int RGWRados::put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
 
   ObjectWriteOperation op;
 
-  if (exclusive)
+  if (exclusive) {
     op.create(true);
+  }
 
   if (objv_tracker) {
     objv_tracker->prepare_op_for_write(&op);
   }
-  if (ofs == -1) {
-    op.write_full(bl);
-  } else {
-    op.write(ofs, bl);
-  }
+
+  op.set_alloc_hint2(bl.length(), bl.length(), ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                                               ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                                               ALLOC_HINT_FLAG_APPEND_ONLY);
+  op.write_full(bl);
   r = ref.ioctx.operate(ref.oid, &op);
   if (r < 0)
     return r;
@@ -7181,7 +7190,7 @@ int RGWRados::put_system_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
  * Returns: 0 on success, -ERR# otherwise.
  */
 
-int RGWRados::aio_put_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
+int RGWRados::aio_put_obj_data(rgw_raw_obj& obj, bufferlist& bl,
 			       off_t ofs, bool exclusive,
                                void **handle)
 {
@@ -7200,6 +7209,16 @@ int RGWRados::aio_put_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
     op.create(true);
 
   if (ofs == -1) {
+    /*
+     * not setting size for allocation hint
+     * assuming first write buffer length is the common chunk size that will be used.
+     * Not using configured chunk size, because that might be modified when pool
+     * alignment requires.
+     */
+    ldout(cct, 20) << "setting rados allocation hint, write_size=" << bl.length() << dendl;
+    op.set_alloc_hint2(-1, bl.length(), ALLOC_HINT_FLAG_SEQUENTIAL_WRITE |
+                       ALLOC_HINT_FLAG_SEQUENTIAL_READ |
+                       ALLOC_HINT_FLAG_IMMUTABLE);
     op.write_full(bl);
   } else {
     op.write(ofs, bl);