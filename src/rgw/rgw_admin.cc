e0e5f760f277db0963f10cffb8d4c2181d4d3ce6
Yehuda Sadeh <yehuda@redhat.com>
2017-08-28 02:33:20 -0700

    rgw: create global http manager
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1058,6 +1058,7 @@ public:
   explicit StoreDestructor(RGWRados *_s) : store(_s) {}
   ~StoreDestructor() {
     RGWStoreManager::close_storage(store);
+    rgw_http_client_cleanup();
   }
 };
 
@@ -2985,6 +2986,7 @@ int main(int argc, const char **argv)
 
   rgw_user_init(store);
   rgw_bucket_init(store->meta_mgr);
+  rgw_http_client_init(g_ceph_context);
 
   StoreDestructor store_destructor(store);
 
