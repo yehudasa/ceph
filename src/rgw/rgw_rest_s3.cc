e28d8458a8251a24fe98ddb220017249f1e19b3b
Yehuda Sadeh <yehuda@redhat.com>
2017-11-17 15:40:25 -0800

    rgw: send totp check to the otp objclass
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -3234,7 +3234,7 @@ static int verify_mfa(RGWRados *store, RGWUserInfo *user, const string& mfa_str,
   }
 
   string& serial = params[0];
-  string& otp = params[1];
+  string& pin = params[1];
 
   auto i = user->mfa_ids.find(serial);
   if (i == user->mfa_ids.end()) {
@@ -3242,6 +3242,13 @@ static int verify_mfa(RGWRados *store, RGWUserInfo *user, const string& mfa_str,
     return -EACCES;
   }
 
+  int ret = store->check_mfa(user->user_id, serial, pin);
+  if (ret < 0) {
+    ldout(store->ctx(), 20) << "NOTICE: failed to check MFA, serial=" << serial << dendl;
+    return -EACCES;
+  }
+
+#warning clean me up
 #if 0
   string& seed = i->second;
 
