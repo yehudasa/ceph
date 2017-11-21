184791eab74cb91fc17681a71ac66b416bb2c1e1
Yehuda Sadeh <yehuda@redhat.com>
2017-11-21 15:11:34 -0800

    rgw: initial work to add otp as metadata provider
    To allow transparent multisite sync
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -28,6 +28,7 @@
 
 #include "rgw_user.h"
 #include "rgw_bucket.h"
+#include "rgw_otp.h"
 #include "rgw_rados.h"
 #include "rgw_acl.h"
 #include "rgw_acl_s3.h"
@@ -2979,6 +2980,7 @@ int main(int argc, const char **argv)
 
   rgw_user_init(store);
   rgw_bucket_init(store->meta_mgr);
+  rgw_otp_init(store);
 
   StoreDestructor store_destructor(store);
 
--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -30,6 +30,7 @@
 #include "rgw_common.h"
 #include "rgw_rados.h"
 #include "rgw_user.h"
+#include "rgw_otp.h"
 #include "rgw_period_pusher.h"
 #include "rgw_realm_reloader.h"
 #include "rgw_rest.h"
@@ -353,6 +354,7 @@ int main(int argc, const char **argv)
 
   rgw_user_init(store);
   rgw_bucket_init(store->meta_mgr);
+  rgw_otp_init(store);
   rgw_log_usage_init(g_ceph_context, store);
 
   RGWREST rest;
