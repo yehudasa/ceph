b337ac871ef4af6e12b3ef0bd14a0bcd34fedd7c
Yehuda Sadeh <yehuda@redhat.com>
2018-10-10 16:55:45 -0700

    rgw, cls_rgw: propagate storage class to the index
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/cls/rgw/cls_rgw_types.h
+++ src/cls/rgw/cls_rgw_types.h
@@ -101,12 +101,13 @@ struct rgw_bucket_dir_entry_meta {
   string content_type;
   uint64_t accounted_size;
   string user_data;
+  string storage_class;
 
   rgw_bucket_dir_entry_meta() :
   category(0), size(0), accounted_size(0) { }
 
   void encode(bufferlist &bl) const {
-    ENCODE_START(5, 3, bl);
+    ENCODE_START(6, 3, bl);
     encode(category, bl);
     encode(size, bl);
     encode(mtime, bl);
@@ -116,10 +117,11 @@ struct rgw_bucket_dir_entry_meta {
     encode(content_type, bl);
     encode(accounted_size, bl);
     encode(user_data, bl);
+    encode(storage_class, bl);
     ENCODE_FINISH(bl);
   }
   void decode(bufferlist::const_iterator &bl) {
-    DECODE_START_LEGACY_COMPAT_LEN(5, 3, 3, bl);
+    DECODE_START_LEGACY_COMPAT_LEN(6, 3, 3, bl);
     decode(category, bl);
     decode(size, bl);
     decode(mtime, bl);
@@ -134,6 +136,8 @@ struct rgw_bucket_dir_entry_meta {
       accounted_size = size;
     if (struct_v >= 5)
       decode(user_data, bl);
+    if (struct_v >= 6)
+      decode(storage_class, bl);
     DECODE_FINISH(bl);
   }
   void dump(Formatter *f) const;
