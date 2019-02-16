b01e932d8a4fa0754037bf9f021b4f2f67f7218a
Yehuda Sadeh <yehuda@redhat.com>
2019-02-15 16:06:51 -0800

    rgw: bucket sync: integrate bucket sync decisions
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1313,6 +1313,14 @@ struct rgw_bucket_shard {
   }
 };
 
+inline ostream& operator<<(ostream& out, const rgw_bucket_shard& bs) {
+  if (bs.shard_id <= 0) {
+    return out << bs.bucket;
+  }
+
+  return out << bs.bucket << ":" << bs.shard_id;
+}
+
 struct rgw_bucket_placement {
   rgw_placement_rule placement_rule;
   rgw_bucket bucket;
@@ -1397,6 +1405,7 @@ inline ostream& operator<<(ostream& out, const RGWBucketIndexType &index_type)
 }
 
 struct RGWBucketSyncPolicy;
+class RGWSI_Zone;
 
 struct RGWBucketInfo {
   enum BIShardsHashType {
@@ -1471,6 +1480,8 @@ struct RGWBucketInfo {
   void set_sync_policy(RGWBucketSyncPolicy&& policy);
 
   bool empty_sync_policy() const;
+  bool bucket_is_sync_source(const string& zone_id) const;
+  bool bucket_datasync_enabled(const RGWSI_Zone *zone_svc) const;
 
   RGWBucketInfo();
   ~RGWBucketInfo();
