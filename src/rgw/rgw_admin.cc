c8807d03c3390ccb8411523c6700695cf4638fb0
Yehuda Sadeh <yehuda@redhat.com>
2020-04-22 03:40:33 -0700

    rgw-admin: si provider list + fetch
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -60,6 +60,7 @@ extern "C" {
 #include "rgw_bucket_sync.h"
 #include "rgw_sync_checkpoint.h"
 #include "rgw_lua.h"
+#include "rgw_sync_info.h"
 
 #include "services/svc_sync_modules.h"
 #include "services/svc_cls.h"
@@ -775,6 +776,8 @@ enum class OPT {
   SCRIPT_PACKAGE_ADD,
   SCRIPT_PACKAGE_RM,
   SCRIPT_PACKAGE_LIST
+  SI_PROVIDER_LIST,
+  SI_PROVIDER_FETCH,
 };
 
 }
@@ -994,6 +997,8 @@ static SimpleCmd::Commands all_cmds = {
   { "script-package add", OPT::SCRIPT_PACKAGE_ADD },
   { "script-package rm", OPT::SCRIPT_PACKAGE_RM },
   { "script-package list", OPT::SCRIPT_PACKAGE_LIST },
+  { "si provider list", OPT::SI_PROVIDER_LIST },
+  { "si provider fetch", OPT::SI_PROVIDER_FETCH },
 };
 
 static SimpleCmd::Aliases cmd_aliases = {
@@ -3225,6 +3230,8 @@ int main(int argc, const char **argv)
   ceph::timespan opt_retry_delay_ms = std::chrono::milliseconds(2000);
   ceph::timespan opt_timeout_sec = std::chrono::seconds(60);
 
+  std::optional<string> opt_provider;
+
   SimpleCmd cmd(all_cmds, cmd_aliases);
 
   for (std::vector<const char*>::iterator i = args.begin(); i != args.end(); ) {
@@ -3864,11 +3871,9 @@ int main(int argc, const char **argv)
 			 OPT::ROLE_POLICY_GET,
 			 OPT::RESHARD_LIST,
 			 OPT::RESHARD_STATUS,
-       OPT::PUBSUB_TOPICS_LIST,
-       OPT::PUBSUB_TOPIC_GET,
-       OPT::PUBSUB_SUB_GET,
-       OPT::PUBSUB_SUB_PULL,
-       OPT::SCRIPT_GET,
+                         OPT::SCRIPT_GET,
+			 OPT::SI_PROVIDER_LIST,
+			 OPT::SI_PROVIDER_FETCH,
   };
 
 
@@ -9418,6 +9423,48 @@ next:
     return EPERM;
 #endif
   }
+
+ if (opt_cmd == OPT::SI_PROVIDER_LIST) {
+   auto providers = store->ctl()->si.mgr->list_sip();
+   {
+     Formatter::ObjectSection top_section(*formatter, "result");
+     encode_json("providers", providers, formatter.get());
+   }
+   formatter->flush(cout);
+ }
+
+ if (opt_cmd == OPT::SI_PROVIDER_FETCH) {
+   if (!opt_provider) {
+     cerr << "ERROR: --provider not specified" << std::endl;
+     return EINVAL;
+   }
+   auto provider = store->ctl()->si.mgr->find_sip(*opt_provider);
+   if (!provider) {
+     cerr << "ERROR: sync info provider not found" << std::endl;
+     return ENOENT;
+   }
+
+   SIProvider::fetch_result result;
+   int r = provider->fetch(marker, max_entries, &result);
+   if (r < 0) {
+     cerr << "ERROR: failed to fetch entries: " << cpp_strerror(-r) << std::endl;
+     return -r;
+   }
+
+   {
+     Formatter::ObjectSection top_section(*formatter, "result");
+     encode_json("more", result.more, formatter.get());
+     encode_json("done", result.done, formatter.get());
+
+     Formatter::ArraySection as(*formatter, "entries");
+
+     for (auto& e : result.entries) {
+       Formatter::ObjectSection hs(*formatter, "handler");
+       encode_json("key", e.key, formatter.get());
+     }
+   }
+   formatter->flush(cout);
+ }
 
   return 0;
 }
