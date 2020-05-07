2dd73eb70098cc8b141cff4f8adf75d9d78b48fe
Yehuda Sadeh <yehuda@redhat.com>
2020-05-07 06:35:25 -0700

    rgw: sip: manager holds provider generator
    enable the ability to create different instances of the same provider
    type (e.g., for bucket instance).
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -3265,7 +3265,8 @@ int main(int argc, const char **argv)
   std::optional<string> opt_mode;
   std::optional<rgw_user> opt_dest_owner;
 
-  std::optional<string> opt_provider;
+  std::optional<string> opt_sip;
+  std::optional<string> opt_sip_instance;
   std::optional<SIProvider::stage_id_t> opt_stage_id;
 
   SimpleCmd cmd(all_cmds, cmd_aliases);
@@ -3679,8 +3680,10 @@ int main(int argc, const char **argv)
     } else if (ceph_argparse_witharg(args, i, &val, "--dest-owner", (char*)NULL)) {
       opt_dest_owner.emplace(val);
       opt_dest_owner = val;
-    } else if (ceph_argparse_witharg(args, i, &val, "--provider", (char*)NULL)) {
-      opt_provider = val;
+    } else if (ceph_argparse_witharg(args, i, &val, "--sip", "--si-provider", (char*)NULL)) {
+      opt_sip = val;
+    } else if (ceph_argparse_witharg(args, i, &val, "--sip-instance", (char*)NULL)) {
+      opt_sip_instance = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--stage-id", (char*)NULL)) {
       opt_stage_id = val;
     } else if (ceph_argparse_binary_flag(args, i, &detail, NULL, "--detail", (char*)NULL)) {
@@ -9236,11 +9239,11 @@ next:
  }
 
  if (opt_cmd == OPT::SI_PROVIDER_FETCH) {
-   if (!opt_provider) {
-     cerr << "ERROR: --provider not specified" << std::endl;
+   if (!opt_sip) {
+     cerr << "ERROR: --sip not specified" << std::endl;
      return EINVAL;
    }
-   auto provider = store->ctl()->si.mgr->find_sip(*opt_provider);
+   auto provider = store->ctl()->si.mgr->find_sip(*opt_sip, opt_sip_instance);
    if (!provider) {
      cerr << "ERROR: sync info provider not found" << std::endl;
      return ENOENT;
