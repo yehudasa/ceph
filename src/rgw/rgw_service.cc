edac6659d86296cffb741700c86d30fd1f0dc397
Yehuda Sadeh <yehuda@redhat.com>
2020-08-21 07:04:18 +0000

    rgw: ctl.remote: a new ctl module for remote connections
    In order to be able to use a more versatile zone connections config
    (e.g., instead of specifying keys in the zone config, specify a uid
    and use the uid's access keys).
    It is possible to configure a separate connection for zone data and for
    zone sip.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_service.cc
+++ src/rgw/rgw_service.cc
@@ -35,6 +35,7 @@
 #include "rgw_user.h"
 #include "rgw_bucket.h"
 #include "rgw_otp.h"
+#include "rgw_remote.h"
 
 #include "rgw_sync_info.h"
 #include "rgw_sip_meta.h"
@@ -372,6 +373,7 @@ int RGWCtlDef::init(RGWServices& svc)
                                 svc.bucket_sync,
                                 svc.bi));
   otp.reset(new RGWOTPCtl(svc.zone, svc.otp));
+  remote.reset(new RGWRemoteCtl(svc.zone, user.get()));
 
   RGWBucketMetadataHandlerBase *bucket_meta_handler = static_cast<RGWBucketMetadataHandlerBase *>(meta.bucket.get());
   RGWBucketInstanceMetadataHandlerBase *bi_meta_handler = static_cast<RGWBucketInstanceMetadataHandlerBase *>(meta.bucket_instance.get());
@@ -389,6 +391,8 @@ int RGWCtlDef::init(RGWServices& svc)
 	       svc.datalog_rados->get_log());
 
   otp->init((RGWOTPMetadataHandler *)meta.otp.get());
+
+  remote->init();
 
   si.mgr.reset(new RGWSIPManager());
 
@@ -415,6 +419,7 @@ int RGWCtl::init(RGWServices *_svc)
   user = _ctl.user.get();
   bucket = _ctl.bucket.get();
   otp = _ctl.otp.get();
+  remote = _ctl.remote.get();
 
   si.mgr = _ctl.si.mgr.get();
 
