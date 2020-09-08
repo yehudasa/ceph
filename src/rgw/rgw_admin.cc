5b1fc1c678964d1a81ec01ae3d7cd8ae99e12f5b
Yehuda Sadeh <yehuda@redhat.com>
2020-09-08 07:56:38 +0000

    rgw-admin: use correct connection for remote sip ops
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -3085,7 +3085,7 @@ public:
     }
 
     return new SIProvider_REST_SingleType(cct, this,
-                                          c->data, &http_manager,
+                                          c->sip, &http_manager,
                                           remote_provider_name,
                                           instance,
                                           type_provider);
