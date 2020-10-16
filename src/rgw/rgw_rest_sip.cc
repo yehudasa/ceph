8ba7e1d075c38c5298ad1f22dabb01a7c3634144
Yehuda Sadeh <yehuda@redhat.com>
2020-10-16 12:09:52 -0700

    rgw: sip rest: get marker info
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_rest_sip.cc
+++ src/rgw/rgw_rest_sip.cc
@@ -102,6 +102,61 @@ void RGWOp_SIP_GetStageStatus::send_response() {
   flusher.flush();
 }
 
+void RGWOp_SIP_GetMarkerInfo::execute() {
+  auto opt_instance = s->info.args.get_std_optional("instance");
+
+  auto sip = store->ctl()->si.mgr->find_sip(provider, opt_instance);
+  if (!sip) {
+    ldout(s->cct, 5) << "ERROR: sync info provider not found" << dendl;
+    http_ret = -ENOENT;
+    return;
+  }
+
+  auto opt_stage_id = s->info.args.get_std_optional("stage-id");
+  if (!opt_stage_id) {
+    ldout(s->cct,  5) << "ERROR: missing 'stage-id' param" << dendl;
+    http_ret = -EINVAL;
+    return;
+  }
+  auto& sid = *opt_stage_id;
+
+  int shard_id;
+  http_ret = s->info.args.get_int("shard-id", &shard_id, 0);
+  if (http_ret < 0) {
+    ldout(s->cct, 5) << "ERROR: invalid 'shard-id' param: " << http_ret << dendl;
+    return;
+  }
+
+  auto marker_handler = store->svc()->sip_marker->get_handler(sip);
+  if (!marker_handler) {
+    ldout(s->cct, 0) << "ERROR: can't get sip marker handler" << dendl;
+    http_ret = -EIO;
+    return;
+  }
+
+  http_ret = marker_handler->get_info(sid, shard_id, &sinfo);
+  if (http_ret < 0) {
+    ldout(s->cct, 0) << "ERROR: failed to fetch marker handler stage marker info: " << cpp_strerror(-http_ret) << dendl;
+    return;
+  }
+
+}
+
+void RGWOp_SIP_GetMarkerInfo::send_response() {
+  set_req_state_err(s, http_ret);
+  dump_errno(s);
+  end_header(s);
+
+  if (http_ret < 0)
+    return;
+
+  {
+    Formatter::ObjectSection top_section(*s->formatter, "result");
+    encode_json("info", sinfo, s->formatter);
+  }
+  flusher.flush();
+}
+
 void RGWOp_SIP_List::execute() {
   providers = store->ctl()->si.mgr->list_sip();
 }
@@ -232,6 +287,11 @@ RGWOp *RGWHandler_SIP::op_get() {
   if (s->info.args.exists("status")) {
     return new RGWOp_SIP_GetStageStatus(std::move(*provider));
   }
+
+  if (s->info.args.exists("marker-info")) {
+    return new RGWOp_SIP_GetMarkerInfo(std::move(*provider));
+  }
+
   return new RGWOp_SIP_Fetch(std::move(*provider));
 }
 
