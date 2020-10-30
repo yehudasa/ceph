fef9fccfd2d3e7c46e9133546ba4abcad865a703
Yehuda Sadeh <yehuda@redhat.com>
2020-10-30 15:37:22 -0700

    rgw: drop the base64 encoding for sip markers
    No need to convert markers to base64. Was done to help with development,
    but at this point it's easier to keep same markers for legacy and new.
    This is needed for auto trim to work.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -1143,9 +1143,6 @@ public:
     }
 
     if (marker.sip_name == "data.inc") {
-      if (!marker.marker.empty()) {
-        marker.marker = rgw::from_base64(marker.marker);
-      }
       marker.sip_name =  get_sip_name();
       return 0;
     }
@@ -1375,9 +1372,6 @@ public:
 
     if (marker.sip_name.empty() ||
         marker.sip_name == "legacy/data.inc") {
-      if (!marker.marker.empty()) {
-        marker.marker = rgw::to_base64(marker.marker);
-      }
       marker.sip_name =  sip_name;
       return 0;
     }
