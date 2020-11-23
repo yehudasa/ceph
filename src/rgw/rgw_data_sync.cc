886f7d60c111ed501f307127f6e05238a2644db5
Yehuda Sadeh <yehuda@redhat.com>
2020-11-23 11:46:07 -0800

    rgw: bucket sync: object is versioned when it has versioned epoch
    There are still cases where we'd get versioned objects without
    versioned epoch, but for these we would expect a later operation
    to provide the epoch.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -4571,7 +4571,7 @@ struct bucket_list_entry {
   RGWModifyOp get_modify_op() const {
     if (delete_marker) {
       return CLS_RGW_OP_LINK_OLH_DM;
-    } else if (!key.instance.empty() && key.instance != "null") {
+    } else if (versioned_epoch) {
       return CLS_RGW_OP_LINK_OLH;
     } else {
       return CLS_RGW_OP_ADD;
@@ -4579,8 +4579,7 @@ struct bucket_list_entry {
   }
 
   bool is_versioned() const {
-    return (delete_marker ||
-            (!key.instance.empty() && key.instance != "null"));
+    return !!versioned_epoch;
   }
 };
 
