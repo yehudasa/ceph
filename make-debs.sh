4f8da0572fa711879a94f509dc0038322caaefce
Alex Marangone <amarangone@digitalocean.com>
2022-03-21 08:40:50 -0700

    make-debs.sh: do not inhibit from building dbg packages

--- make-debs.sh
+++ make-debs.sh
@@ -46,8 +46,6 @@ tar -C $releasedir -jxf $releasedir/ceph_$vers.orig.tar.bz2
 #
 cp -a debian $releasedir/ceph-$vers/debian
 cd $releasedir
-perl -ni -e 'print if(!(/^Package: .*-dbg$/../^$/))' ceph-$vers/debian/control
-perl -pi -e 's/--dbg-package.*//' ceph-$vers/debian/rules
 #
 # always set the debian version to 1 which is ok because the debian
 # directory is included in the sources and the upstream version will
