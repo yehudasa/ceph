5f38c7d4294c607dbbf7c663b8f775d82690b93f
Tyler Brekke <tbrekke@digitalocean.com>
2022-05-20 15:04:58 -0700

    pg_autoscaler: fix bad merge

--- src/pybind/mgr/pg_autoscaler/module.py
+++ src/pybind/mgr/pg_autoscaler/module.py
@@ -189,7 +189,8 @@ class PgAutoscaler(MgrModule):
         """
         osdmap = self.get_osdmap()
         pools = osdmap.get_pools_by_name()
-        ps, root_map = self._get_pool_status(osdmap, pools)
+        profile = self.autoscale_profile
+        ps, root_map = self._get_pool_status(osdmap, pools, profile)
 
         if format in ('json', 'json-pretty'):
             return 0, json.dumps(ps, indent=4, sort_keys=True), ''
