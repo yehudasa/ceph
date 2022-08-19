4a5239098dab01cd7e13187fe756618b5c87374f
Joshua Baergen <jbaergen@digitalocean.com>
2022-08-19 12:30:04 -0600

    tools/rbd: Re-arrange the added DO logic for ignoring namespaces in volume specs.
    This restores/preserves the original logic in the case that
    rbd_validate_namespace=true.

--- src/tools/rbd/Utils.cc
+++ src/tools/rbd/Utils.cc
@@ -148,12 +148,25 @@ int extract_spec(const std::string &spec, std::string *pool_name,
   }
 
 
-  if (match[2].matched) {
-    if (!g_ceph_context->_conf.get_val<bool>("rbd_validate_namespace")) {
+  if (!g_ceph_context->_conf.get_val<bool>("rbd_validate_namespace")) {
+    // XXX DO-specific logic - our externally-facing volumes take the form <user_id>/<volume_id>,
+    // and thus the "namespace" is actually the user_id. When namespace support is disabled we
+    // assume that this is what we're dealing with if a "namespace" is given.
+    if (match[2].matched) {
       if (name != nullptr) {
         *name = match[2];
       }
-    } else {
+    }
+
+    if (name != nullptr) {
+      if (spec_validation == SPEC_VALIDATION_NONE) {
+        *name = *name + "/";
+      }
+      *name = *name + match.str(3);
+    }
+  } else {
+    // Original upstream logic
+    if (match[2].matched) {
       if (namespace_name != nullptr) {
         *namespace_name = match[2];
       } else {
@@ -162,13 +175,10 @@ int extract_spec(const std::string &spec, std::string *pool_name,
         return -EINVAL;
       }
     }
-  }
 
-  if (name != nullptr) {
-    if (spec_validation == SPEC_VALIDATION_NONE) {
-      *name = *name + "/";
+    if (name != nullptr) {
+      *name = match[3];
     }
-    *name = *name + match.str(3);
   }
 
   if (match[4].matched) {
