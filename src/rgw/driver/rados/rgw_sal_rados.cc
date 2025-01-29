f98c4dc61ea1c349df0aa89d76cf7c5e16daf335
Yehuda Sadeh <ysadehwe@ibm.com>
2025-01-29 13:13:45 -0500

    rgw: rework object snap representation
    Beforehand the snap_id field in the rgw_obj_key to identify whether an
    object instance belong to a specific snapshot. However, this was inconsistent
    with the idea that we don't want to physically represent versioned objects
    (as in non-nulled) without the snap_id. This lead to a lot of inconsistencies.
    Now rgw_obj_key only represents the object instance key: versioned objects
    will not have snap_id set, even if they're in a snapshot. Non-versioned
    objects will have the snap_id set. The RGWObjState now has a snap_id
    field, and a snap_id attr was added to the object's meta.
    
    Signed-off-by: Yehuda Sadeh <ysadehwe@ibm.com>

--- src/rgw/driver/rados/rgw_sal_rados.cc
+++ src/rgw/driver/rados/rgw_sal_rados.cc
@@ -2241,6 +2241,7 @@ int RadosObject::load_obj_state(const DoutPrefixProvider* dpp, optional_yield y,
 
   /* Don't overwrite obj, atomic, or prefetch */
   rgw_obj obj = get_obj();
+
   bool is_atomic = state.is_atomic;
   bool prefetch_data = state.prefetch_data;
 
@@ -2249,6 +2250,11 @@ int RadosObject::load_obj_state(const DoutPrefixProvider* dpp, optional_yield y,
   state.obj = obj;
   state.is_atomic = is_atomic;
   state.prefetch_data = prefetch_data;
+
+  if (state.snap_id != state.obj.key.snap_id) {
+    state.obj.key.try_set_snap_id(state.snap_id);
+  }
+
   return ret;
 }
 
